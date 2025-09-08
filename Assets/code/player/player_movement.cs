371850842e74fe34383b5105449e29bcebf0b3df
iron5050 <benedikt.ingi.a@gmail.com>
2025-09-08 00:39:19 +0000

    first psuh

---
+++ Assets/code/player/player_movement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class player_movement : MonoBehaviour
+{
+    public float movespeed = 3.0f;
+    public float turnspeed = 8.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float turn = Input.GetAxis("Horizontal");
+        float move = Input.GetAxis("Vertical");
+
+        transform.Rotate(Vector3.up, turn * turnspeed * Time.deltaTime);
+
+
+        Vector3 pos = transform.position;
+        pos += transform.forward * move * movespeed * Time.deltaTime;
+        transform.position = pos;
+    }
+}
