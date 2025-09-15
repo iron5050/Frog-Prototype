9a9bab93b38aca6053fd9622db8d89cda69d7fac
iron5050 <benedikt.ingi.a@gmail.com>
2025-09-15 12:33:30 +0000

    movement based on player (not camera)

--- Assets/Scripts/Player Frog code/PLayer_Movement.cs
+++ Assets/Scripts/Player Frog code/PLayer_Movement.cs
@@ -1,12 +1,24 @@
 using UnityEngine;
 
-public class PLayer_Movement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    // Update is called once per frame
+    public float speed;
+    public float rotationSpeed;
+
     void Update()
     {
-       
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+        movementDirection.Normalize();
+
+        transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);
+
+        if (movementDirection != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
     }
-
 }
-
--- Assets/Scripts/Player Frog code/ThirdPersonCamera.cs
+++ Assets/Scripts/Player Frog code/ThirdPersonCamera.cs
@@ -4,34 +4,38 @@ using UnityEngine;
 
 public class ThirdPersonCamera : MonoBehaviour
 {
-    [Header("refrence")]
-    public Transform orientation;
-    public Transform player;
-    public Transform playerobj;
-    public Rigidbody rb;
+    //[Header("refrence")]
+    //public Transform orientation;
+    //public Transform player;
+    //public Transform playerobj;
+    //public Rigidbody rb;
 
-    public float rotationspeed;
+    //public float rotationspeed;
 
     private void Start()
     {
         //make cursor invisible
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+
+
+
     }
 
     //this makes the player mesh face direction of arrow keys based on where the camera is looking.
-    private void Update()
-    {
-        //rotation orientation
-        Vector3 viewDir = player.position - new Vector3(transform.position.x, transform.position.y, transform.position.z);
-        orientation.forward = viewDir.normalized;
+    //private void Update()
+    //{
 
-        //rotate player object
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
+    //    //rotation orientation
+    //    Vector3 viewDir = player.position - new Vector3(transform.position.x, transform.position.y, transform.position.z);
+    //    orientation.forward = viewDir.normalized;
 
-        if (inputDir != Vector3.zero)
-            playerobj.forward = Vector3.Slerp(playerobj.forward, inputDir.normalized, Time.deltaTime * rotationspeed);
-    }
+    //    //rotate player object
+    //    float horizontalInput = Input.GetAxis("Horizontal");
+    //    float verticalInput = Input.GetAxis("Vertical");
+    //    Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+    //    if (inputDir != Vector3.zero)
+    //        playerobj.forward = Vector3.Slerp(playerobj.forward, inputDir.normalized, Time.deltaTime * rotationspeed);
+    //}
 }
